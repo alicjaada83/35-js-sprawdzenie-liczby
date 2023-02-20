e84924aa78dbc75b9cf836a749a2adc98afe9e4b
alicjaada83 <alicja.adamczyk83@gmail.com>
2023-02-20 19:15:23 +0100

    odpowiedz1

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let number = 203;
+if (number >= 100 && number <= 200) {
+    console.log('Liczba znajduje się w przedziale');
+} else {
+        console.log('Liczba NIE znajduje się w przedziale');
+    }
